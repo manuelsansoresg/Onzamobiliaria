15f7fd8e75ef870429a9d675d6690ce4760d630f
manuel <manuelsansoresg@gmail.com>
2019-10-28 15:40:41 -0600

    busqueda idependiente

--- resources/views/propiedad/index.blade.php
+++ resources/views/propiedad/index.blade.php
@@ -142,53 +142,50 @@
     $(function() {
 
        $('#mobiliaria thead tr').clone(true).appendTo( '#mobiliaria thead' );
-            $('#mobiliaria thead tr:eq(1) th').each( function (i) {
-                var title = $(this).text();
-                $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-                
-                $( 'input', this ).on( 'keyup change', function () {
-                if ( table.column(i).search() !== this.value ) {
-                table
-                .column(i)
-                .search( this.value )
-                .draw();
-            }
-            } );
+        $('#mobiliaria thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        
+        $( 'input', this ).on( 'keyup change', function () {
+        if ( table.column(i).search() !== this.value ) {
+        table
+        .column(i)
+        .search( this.value )
+        .draw();
+        }
+        } );
+        } );
+        
+        var table = $('#mobiliaria').DataTable( {
+        orderCellsTop: true,
+        fixedHeader: true,
+       "scrollX": true,
+       language: {
+    "decimal": "",
+    "emptyTable": "No hay información",
+    "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+    "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+    "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+    "infoPostFix": "",
+    "thousands": ",",
+    "lengthMenu": "Mostrar _MENU_ Entradas",
+    "loadingRecords": "Cargando...",
+    "processing": "Procesando...",
+    "search": "Buscar:",
+    "zeroRecords": "Sin resultados encontrados",
+    "paginate": {
+    "first": "Primero",
+    "last": "Ultimo",
+    "next": "Siguiente",
+    "previous": "Anterior"
+    }
+    },
         } );
 
 
 
 
-        var table = $('#mobiliaria').DataTable({
-            'fixedHeader': true,
-            'paging': true,
-            'lengthChange': false,
-            'searching': false,
-            'ordering': true,
-            'info': true,
-            'autoWidth': false,
-            "scrollX": true,
-            language: {
-                "decimal": "",
-                "emptyTable": "No hay información",
-                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-                "infoPostFix": "",
-                "thousands": ",",
-                "lengthMenu": "Mostrar _MENU_ Entradas",
-                "loadingRecords": "Cargando...",
-                "processing": "Procesando...",
-                "search": "Buscar:",
-                "zeroRecords": "Sin resultados encontrados",
-                "paginate": {
-                    "first": "Primero",
-                    "last": "Ultimo",
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            },
-        })
+        /*  */
 
         
 
