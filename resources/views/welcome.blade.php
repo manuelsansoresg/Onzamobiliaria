2c362eb8a76bfbdc8018f61d8c2456137c6ec323
kinggasper <gem_88@hotmail.com>
2019-11-16 19:38:33 -0600

    cAMBIOS
    MENU

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,29 @@
 
         <div class="col-12 col-md-2">
             <div class="card text-center">
-                <a href="/admin/pago">
-                    <i class="fas fa-hand-holding-usd fa-3x mt-3"></i>
+                <a href="/admin/clientes">
+                    <i class="fas fa-box-open fa-3x mt-3 text-primary"></i>
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a href="/admin/clientes" class="text-body">
+                            <span class="h6">PROSPECCION DE INMUEBLES</span>
+                        </a>
+                    </h5>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-2">
+            <div class="card text-center">
+                <a href="/admin/propiedad">
+                    <i class="fas fa-home fa-3x mt-3 text-primary"></i>
                 </a>
                 <div class="card-body">
                     <h5 class="card-title">
                         <a href="" class="text-body">
-                            <span class="h6">METODO PAGO</span>
+                        <span class="h6">PROPIEDADES</span>
                         </a>
                     </h5>
 
@@ -38,83 +54,27 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-md-2">
-            <div class="card text-center">
-                <a href="/admin/clientes">
-                    <i class="fas fa-box-open fa-3x mt-3 text-primary"></i>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="/admin/clientes" class="text-body">
-                            <span class="h6">PROSPECCION DE INMUEBLES</span>
-                        </a>
-                    </h5>
 
-                </div>
-            </div>
-        </div>
         <div class="col-12 col-md-2">
             <div class="card text-center">
-                <a href="/admin/propiedad">
-                    <i class="fas fa-home fa-3x mt-3 text-primary"></i>
+                <a href="/admin/pago">
+                    <i class="fas fa-hand-holding-usd fa-3x mt-3"></i>
                 </a>
                 <div class="card-body">
                     <h5 class="card-title">
                         <a href="" class="text-body">
-                        <span class="h6">PROPIEDADES</span>
+                            <span class="h6">CATALOGOS</span>
                         </a>
                     </h5>
 
                 </div>
             </div>
-        </div>
-
-        <div class="col-12 col-md-2">
-            <div class="card text-center">
-                <a href="/admin/seguimiento-asesores">
-                    <i class="fas fa-clipboard fa-3x mt-3 text-primary"></i>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="" class="text-body">
-                        <span class="h6">SEGUIMIENTOS</span>
-                        </a>
-                    </h5>
-
-                </div>
-            </div>
-        </div>
+        </div>       
+        
+       
         @endrole
         @role('asesor')
-        <div class="col-12 col-md-2">
-            <div class="card text-center">
-                <a href="/admin/clientes">
-                    <i class="fas fa-box-open fa-3x mt-3 text-primary"></i>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="/admin/clientes" class="text-body">Clientes</a>
-                    </h5>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-md-2">
-            <div class="card text-center">
-                <a href="/admin/seguimiento-asesores">
-                    <i class="fas fa-clipboard fa-3x mt-3 text-primary"></i>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="" class="text-body">
-                            Seguimiento
-                        </a>
-                    </h5>
-
-                </div>
-            </div>
-        </div>
+        
         <div class="col-12 col-md-2">
             <div class="card text-center">
                 <a href="/admin/clientes">
@@ -130,6 +90,7 @@
                 </div>
             </div>
         </div>
+
         <div class="col-12 col-md-2">
             <div class="card text-center">
                 <a href="/admin/propiedad">
@@ -146,6 +107,21 @@
             </div>
         </div>
 
+        <div class="col-12 col-md-2">
+            <div class="card text-center">
+                <a href="/admin/clientes">
+                    <i class="fas fa-box-open fa-3x mt-3 text-primary"></i>
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a href="/admin/clientes" class="text-body">
+                            <span class="h6">PROSPECTO VENTA/RENTA</span>
+                        </a>
+                    </h5>
+
+                </div>
+            </div>
+        </div>
         @endrole
 
 
