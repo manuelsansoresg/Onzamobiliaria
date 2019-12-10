0dbd74228d43174feb409c58a5c4f68e3478a1b0
kinggasper <gem_88@hotmail.com>
2019-12-09 18:59:01 -0600

    cambios
    cambios

--- resources/views/operaciones/index.blade.php
+++ resources/views/operaciones/index.blade.php
@@ -3,67 +3,78 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="row mt-3">
-        <div class="col-12 text-right">
-            <a href="/admin/operaciones/create" class="btn btn-success btn-sm  pull-right">
-                <i class="fas fa-plus-circle"></i> &nbsp; Nuevo
-            </a>
-        </div>
+    <div class="row mt-3">        
         <div class="col-12 mt-3">
             @include('flash::message')
         </div>
     </div>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center">        
         <div class="col-12">
-            <table id="mobiliaria" class="table table-striped table-bordered dt-responsive nowrap">
-                <thead>
-                    <tr>
-                        <th> <span class="small font-weight-bold">#</span></th>
-                        <th> <span class="small font-weight-bold">DESCRIPCION</span> </th>
-                        <th> <span class="small font-weight-bold">ESTATUS</span> </th>        
-                        <th style="width: 60px"></th>
-                    </tr>
-                </thead>
-                <tbody>                        
-                        @foreach ($operations as $operation)
-                    <tr>
-                        <td><span class="small">{{ $operation->id  }}</span></td>
-                        <td><span class="small">{{ $operation->description  }}</span></td>
-                        <td><span class="small">
-                            @if($operation->status == 0)
-                                <i class="fas fa-ban text-danger"></i>
-                                <span>Inactivo</span>
-                            @else
-                                <i class="fas fa-ban text-success"></i>
-                                <span>Activo</span>
-                            @endif        
-                            </span>
-                        </td>
-                        <td>
-                            {{ Form::open(['route' => ['operaciones.destroy', $operation->id ],'class' => 'form-inline', 'method' => 'DELETE' ])}}
-                            <a href="{{route('operaciones.edit', $operation->id)}}" class="btn btn-sm btn-primary">
-                                <i class="far fa-edit"></i>
-                            </a>
-                            @if($operation->status == 0)
-                                <a href="/admin/operaciones/status/{{ $operation->id }}/1" class="btn btn-success">
-                                    <i class="fas fa-ban text-white"></i>
-                                </a>
-                            @else
-                                <a href="/admin/operaciones/status/{{ $operation->id }}/0" class="btn btn-warning">
-                                    <i class="fas fa-ban"></i>
-                                </a>
-                            @endif
-                            @role('admin')
-                                <button onclick="return confirm('¿Deseas eliminar el elemento?')" class="btn btn-danger btn-sm ml-1">
-                                    <i class="far fa-trash-alt"></i>
-                                </button>
-                            @endrole
-                            {{ Form::close() }}
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h5 class="mr-auto">LISTADO DE OPERACIONES</h5>
+                        <div>
+                            <a href="/admin/operaciones/create" class="btn btn-success btn-sm  pull-right"><i class="fas fa-plus-circle"></i> AGREGAR</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <table id="mobiliaria" class="table table-striped table-bordered dt-responsive nowrap">
+                                <thead>
+                                    <tr>
+                                        <th> <span class="small font-weight-bold">#</span></th>
+                                        <th> <span class="small font-weight-bold">DESCRIPCION</span> </th>
+                                        <th> <span class="small font-weight-bold">ESTATUS</span> </th>        
+                                        <th style="width: 60px"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>                        
+                                        @foreach ($operations as $operation)
+                                    <tr>
+                                        <td><span class="small">{{ $operation->id  }}</span></td>
+                                        <td><span class="small">{{ $operation->description  }}</span></td>
+                                        <td><span class="small">
+                                            @if($operation->status == 0)
+                                                <i class="fas fa-ban text-danger"></i>
+                                                <span>Inactivo</span>
+                                            @else
+                                                <i class="fas fa-ban text-success"></i>
+                                                <span>Activo</span>
+                                            @endif        
+                                            </span>
+                                        </td>
+                                        <td>
+                                            {{ Form::open(['route' => ['operaciones.destroy', $operation->id ],'class' => 'form-inline', 'method' => 'DELETE' ])}}
+                                            <a href="{{route('operaciones.edit', $operation->id)}}" class="btn btn-sm btn-primary">
+                                                <i class="far fa-edit"></i>
+                                            </a>
+                                            @if($operation->status == 0)
+                                                <a href="/admin/operaciones/status/{{ $operation->id }}/1" class="btn btn-success">
+                                                    <i class="fas fa-ban text-white"></i>
+                                                </a>
+                                            @else
+                                                <a href="/admin/operaciones/status/{{ $operation->id }}/0" class="btn btn-warning">
+                                                    <i class="fas fa-ban"></i>
+                                                </a>
+                                            @endif
+                                            @role('admin')
+                                                <button onclick="return confirm('¿Deseas eliminar el elemento?')" class="btn btn-danger btn-sm ml-1">
+                                                    <i class="far fa-trash-alt"></i>
+                                                </button>
+                                            @endrole
+                                            {{ Form::close() }}
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -77,7 +88,7 @@
             "bSearchable": true,
             "bFilter": true,
             responsive: true,
-            "pageLength": 5,
+            "pageLength": 10,
 
             language: {
                 "decimal": "",
--- resources/views/portales/index.blade.php
+++ resources/views/portales/index.blade.php
@@ -3,68 +3,79 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="row mt-3">
-        <div class="col-12 text-right">
-            <a href="/admin/portales/create" class="btn btn-success btn-sm  pull-right">
-                <i class="fas fa-plus-circle"></i> &nbsp; Nuevo
-            </a>
-        </div>
+    <div class="row mt-3">        
         <div class="col-12 mt-3">
             @include('flash::message')
         </div>    
     </div>
     <div class="row justify-content-center">
-        <div class="col-12 col-md-8  mt-3">
-            <table id="portales" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
-                <thead>
-                    <tr>
-                        <th> <span class="small font-weight-bold">#</span></th>
-                        <th> <span class="small font-weight-bold">DESCRIPCION</span> </th>
-                        <th> <span class="small font-weight-bold">ESTATUS</span> </th>        
-                        <th style="width: 60px"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($ads as $ad)
-                    <tr>
-                        <td><span class="small">{{ $ad->id  }}</span></td>
-                        <td><span class="small">{{ $ad->description  }}</span></td>
-                        <td><span class="small">
-                            @if($ad->status == 0)
-                                <i class="fas fa-ban text-danger"></i>
-                                <span>Inactivo</span>
-                            @else
-                                <i class="fas fa-ban text-success"></i>
-                                <span>Activo</span>
-                            @endif        
-                            </span>
-                        </td>
-                        <td>
-                            {{ Form::open(['route' => ['portales.destroy', $ad->id ],'class' => 'form-inline', 'method' => 'DELETE' ])}}
-                            <a href="{{route('portales.edit', $ad->id)}}" class="btn btn-sm btn-primary">
-                                <i class="far fa-edit"></i>
-                            </a>
-                            @if($ad->status == 0)
-                                <a href="/admin/portales/status/{{ $ad->id }}/1" class="btn btn-success">
-                                    <i class="fas fa-ban text-white"></i>
-                                </a>
-                            @else
-                                <a href="/admin/portales/status/{{ $ad->id }}/0" class="btn btn-warning">
-                                    <i class="fas fa-ban"></i>
-                                </a>
-                            @endif
-                            @role('admin')
-                                <button onclick="return confirm('¿Deseas eliminar el elemento?')" class="btn btn-danger btn-sm ml-1">
-                                    <i class="far fa-trash-alt"></i>
-                                </button>
-                            @endrole
-                            {{ Form::close() }}
-                        </td>
-                        </tr>
-                        @endforeach
-                    </tr>
-                </tbody>
-            </table>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h5 class="mr-auto">LISTADO DE TIPO DE PORTALES</h5>
+                        <div>
+                            <a href="/admin/portales/create" class="btn btn-success btn-sm  pull-right"><i class="fas fa-plus-circle"></i> AGREGAR</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <table id="portales" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th> <span class="small font-weight-bold">#</span></th>
+                                        <th> <span class="small font-weight-bold">DESCRIPCION</span> </th>
+                                        <th> <span class="small font-weight-bold">ESTATUS</span> </th>        
+                                        <th style="width: 60px"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($ads as $ad)
+                                    <tr>
+                                        <td><span class="small">{{ $ad->id  }}</span></td>
+                                        <td><span class="small">{{ $ad->description  }}</span></td>
+                                        <td><span class="small">
+                                            @if($ad->status == 0)
+                                                <i class="fas fa-ban text-danger"></i>
+                                                <span>Inactivo</span>
+                                            @else
+                                                <i class="fas fa-ban text-success"></i>
+                                                <span>Activo</span>
+                                            @endif        
+                                            </span>
+                                        </td>
+                                        <td>
+                                            {{ Form::open(['route' => ['portales.destroy', $ad->id ],'class' => 'form-inline', 'method' => 'DELETE' ])}}
+                                            <a href="{{route('portales.edit', $ad->id)}}" class="btn btn-sm btn-primary">
+                                                <i class="far fa-edit"></i>
+                                            </a>
+                                            @if($ad->status == 0)
+                                                <a href="/admin/portales/status/{{ $ad->id }}/1" class="btn btn-success">
+                                                    <i class="fas fa-ban text-white"></i>
+                                                </a>
+                                            @else
+                                                <a href="/admin/portales/status/{{ $ad->id }}/0" class="btn btn-warning">
+                                                    <i class="fas fa-ban"></i>
+                                                </a>
+                                            @endif
+                                            @role('admin')
+                                                <button onclick="return confirm('¿Deseas eliminar el elemento?')" class="btn btn-danger btn-sm ml-1">
+                                                    <i class="far fa-trash-alt"></i>
+                                                </button>
+                                            @endrole
+                                            {{ Form::close() }}
+                                        </td>
+                                        </tr>
+                                        @endforeach
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -78,7 +89,7 @@
             "bSearchable": true,
             "bFilter": true,
             responsive: true,
-            "pageLength": 5,
+            "pageLength": 10,
 
             language: {
                 "decimal": "",
